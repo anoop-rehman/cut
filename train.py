0bd2526d19b78f93e1b817d4fd973fd43d5355d1
Anoop Rehman <anoop.rehman@mail.utoronto.ca>
2025-12-22 22:43:49 -0500

    feat: name checkpoint with step count

--- train.py
+++ train.py
@@ -63,14 +63,14 @@ if __name__ == '__main__':
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
                 print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
                 print(opt.name)  # it's useful to occasionally show the experiment name on console
-                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+                save_suffix = 'iter_%d' % total_iters  # Save with step count, overwrites previous checkpoint
                 model.save_networks(save_suffix)
 
             iter_data_time = time.time()
 
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
-            model.save_networks('latest')  # Only save 'latest' to overwrite previous checkpoints
+            model.save_networks('iter_%d' % total_iters)  # Save with step count, overwrites previous checkpoint
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
         model.update_learning_rate()                     # update learning rates at the end of every epoch.
